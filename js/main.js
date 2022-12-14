892ff7b6b071921d90d72dc237206addd3540130
shreyash-x <shreyashjain2002@gmail.com>
2022-12-14 19:27:30 +0530

    Added exp desc handler

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ async function getLog(file, type) {
   } else if (type === "https") {
     output = generateTabs(handleDataHttps(y), type);
     document.getElementById("output-https").innerHTML = output;
+  } else if (type === "descriptor") {
+    output = generateTabs(handleDataDescriptor(y), type);
+    document.getElementById("output-descriptor").innerHTML = output;
   }
 }
 
@@ -74,6 +77,58 @@ function handleDataHttps(data) {
   }
   let stats = `Total Links: ${count} <br>
                 ✖ ${count} problems (0 errors, ${count} warnings)`;
+
+  formatted_data["FINAL_STATS"] = stats;
+  //   console.log(formatted_data);
+  return formatted_data;
+}
+
+function handleDataDescriptor(data) {
+  let logs = data.split("\n");
+  // remove first 6 lines and last 2 lines
+  logs = logs.slice(6, logs.length - 2);
+  let formatted_data = {};
+  let count = 0;
+  for (let i = 0; i < logs.length; i++) {
+    // check if line starts with /
+    if (logs[i].startsWith("/")) {
+      count++;
+      // get filename
+      const head = logs[i].split(":")[0];
+      let text = logs[i].split(":")[1];
+      text = text.trim();
+      const filename = head[i].split("/")[1];
+
+      // get index
+      let index = "";
+      let path = head.split("/");
+      for (let i = 0; i < path.length; i++) {
+        // if path[i] is a string of numbers
+        if (!isNaN(path[i])) {
+          if (index === "") {
+            index = path[i];
+          } else {
+            index += `.${path[i]}`;
+          }
+        }
+      }
+      // add index to text
+      text = `${index}: ${text}`;
+      if (filename in formatted_data) {
+        formatted_data[filename].push(text);
+      } else {
+        formatted_data[filename] = [text];
+      }
+    } else if (logs[i].startsWith("Json Error:")) {
+      if("base" in formatted_data) {
+        formatted_data["base"].push(`0: ${logs[i].split(":")[1].trim()}`);
+      } else {
+        formatted_data["base"] = [`0: ${logs[i].split(":")[1].trim()}`];
+      }
+      count++;
+    }
+  }
+  let stats = `✖ ${count} problems (0 errors, ${count} warnings)`;
 
   formatted_data["FINAL_STATS"] = stats;
   //   console.log(formatted_data);
@@ -140,6 +195,11 @@ function generateHeaders(type) {
     headers = `<th style="width: 15%;">Severity</th>
                 <th>Link</th>`;
   }
+  else if (type === "descriptor") {
+    headers = `<th style="width: 10%;">Index</th>
+                <th style="width: 15%;">Severity</th>
+                <th>Message</th>`;
+  }
   let head = `<thead><tr>${headers}</tr></thead>`;
   return head;
 }
@@ -176,6 +236,12 @@ function generateRow(data, type) {
     // link
     split_data[1] = `<div class="https-link">${split_data[1]}</div>`;
   }
+  else if (type === "descriptor") {
+    split_data = []
+    split_data.push(data.split(":")[0]);
+    split_data.push(`<div class="status-chip background-warning">warning</div>`);
+    split_data.push(data.split(":")[1]);
+  }
 
   for (let i = 0; i < split_data.length; i++) {
     row += `<td class="table-cell">${split_data[i]}</td>`;
@@ -203,6 +269,16 @@ window.toggleHttps = () => {
   }
 };
 
+window.toggleDescriptor = () => {
+  const descriptor = document.getElementById("descriptor");
+  const checkbox = document.getElementById("checkbox-descriptor");
+  if (checkbox.checked) {
+    descriptor.style.display = "inline-block";
+  } else {
+    descriptor.style.display = "none";
+  }
+};
+
 function collapseEslint() {
   const eslint = document.getElementById("eslint");
   const checkboxes = eslint.querySelectorAll(".cb");
@@ -219,46 +295,53 @@ function collapseHttps() {
   }
 }
 
-function toggleWarning() {
-    const checkbox = document.getElementById("checkbox-warning");
-    const warnings = document.getElementsByClassName("is-warning");
+function collapseDescriptor() {
+  const descriptor = document.getElementById("descriptor");
+  const checkboxes = descriptor.querySelectorAll(".cb");
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].checked = false;
+  }
+}
 
-    if (checkbox.checked) {
-        for (let i = 0; i < warnings.length; i++) {
-            warnings[i].style.display = "table-row";
-        }
+function toggleWarning() {
+  const checkbox = document.getElementById("checkbox-warning");
+  const warnings = document.getElementsByClassName("is-warning");
+
+  if (checkbox.checked) {
+    for (let i = 0; i < warnings.length; i++) {
+      warnings[i].style.display = "table-row";
     }
-    else {
-        for (let i = 0; i < warnings.length; i++) {
-            warnings[i].style.display = "none";
-        }
+  } else {
+    for (let i = 0; i < warnings.length; i++) {
+      warnings[i].style.display = "none";
     }
+  }
 }
 
 function toggleError() {
-    const checkbox = document.getElementById("checkbox-error");
-    const errors = document.getElementsByClassName("is-error");
+  const checkbox = document.getElementById("checkbox-error");
+  const errors = document.getElementsByClassName("is-error");
 
-    if (checkbox.checked) {
-        for (let i = 0; i < errors.length; i++) {
-            errors[i].style.display = "table-row";
-        }
+  if (checkbox.checked) {
+    for (let i = 0; i < errors.length; i++) {
+      errors[i].style.display = "table-row";
     }
-    else {
-        for (let i = 0; i < errors.length; i++) {
-            errors[i].style.display = "none";
-        }
+  } else {
+    for (let i = 0; i < errors.length; i++) {
+      errors[i].style.display = "none";
     }
+  }
 }
-
 
 function collapseAll() {
   collapseEslint();
   collapseHttps();
+  collapseDescriptor();
 }
 
 window.collapseEslint = collapseEslint;
 window.collapseHttps = collapseHttps;
+window.collapseDescriptor = collapseDescriptor;
 window.collapseAll = collapseAll;
 
 window.toggleWarning = toggleWarning;
